3bb48fd8be7ed5436679c390330a99d8a6fefe72
Anotonidis <61651107+Anotonidis@users.noreply.github.com>
2023-02-08 23:41:01 +0400

    Update parser_1.py

--- parser_1.py
+++ parser_1.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 
 def main():
    driver = webdriver.Chrome()
-   driver.get("https://skillhub.ru/")
+   driver.get("https://vinyl.ru/catalog/")
    #result = driver.find_elements_by_class_name("main-button")
    #result.click()
 
